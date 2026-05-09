ef92d08b5970807acb615a8f0c0176de47e3027f
mohanraj <mohanraj.m@knowledgeflex.com>
2026-05-09 12:15:03 +0530

    docker file creation done

--- MrMoney.Api/Program.cs
+++ MrMoney.Api/Program.cs
@@ -9,12 +9,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -22,4 +18,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
+app.Run();
 app.Run();
